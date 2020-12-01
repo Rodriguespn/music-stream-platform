df9ed215bd926b078f9915826eaa958a49eb9c0f
Rodriguespn <pedroneverodrigues@gmail.com>
2020-12-01 22:00:12 +0000

    react components created

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-//import HomePage from './HomePage';
+import HomePage from './HomePage';
 
 export default class App extends Component {
     constructor(props) {
@@ -9,8 +9,10 @@ export default class App extends Component {
 
     render() {
         return (
-            <h1>Testing React Code</h1>
-        ); //<HomePage></HomePage>;
+            <div>
+                <HomePage></HomePage>
+            </div>
+        );
     }
 }
 
