6e9f21d1a676e7074efedd2f98c0a8bbe644bd75
Rodriguespn <pedroneverodrigues@gmail.com>
2020-12-20 11:20:34 +0000

    finish video 5

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
 from rest_framework.views import APIView
@@ -22,6 +22,17 @@ class CreateRoomView(APIView):
 
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
-            guest_can_pause = serializer.data.guest_can_pause
-            vostes_to_skip = serializer.data.votes_to_skip
+            guest_can_pause = serializer.data.get('guest_can_pause')
+            votes_to_skip = serializer.data.get('votes_to_skip')
             host = self.request.session.session_key
+            queryset = Room.objects.filter(host=host)
+            if queryset.exists():
+                room = queryset[0]
+                room.guest_can_pause = guest_can_pause
+                room.votes_to_skip = votes_to_skip
+                room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+            else:
+                room = Room(host=host, guest_can_pause = guest_can_pause, votes_to_skip=votes_to_skip)
+                room.save()
+
+            return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
